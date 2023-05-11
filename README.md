0444e630d95b8d01019f284fa7a09372f7f9851e
matd3m0n <real.matd3m0n@gmail.com>
2023-05-11 15:49:27 +0200

    added bits of docs

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# ImageWebApp
-Image processing web app using Python Flask for user-uploaded images and display.
+# Image Processing Web App
+
+The Image Processing Web App is a Flask-based web application that allows users to upload an image, process it using Python, and display the processed image back to the user. It provides a user-friendly interface for uploading images, specifying processing parameters, and visualizing the results.
+
+## Features
+
+- Image upload: Users can upload their images using the provided file upload field.
+- Processing options: Users can specify processing parameters such as angle, sigma, and kernel size.
+- Antialiasing: Users can choose to apply antialiasing to the processed image.
+- Image processing: The uploaded image is processed using the specified parameters.
+- Image display: The original and processed images are displayed to the user for visual comparison.
+
+## Requirements
+
+See `pip_list.txt`
+
+## Usage
+
+This should work on https://www.pythonanywhere.com which allows you to host your
+dynamic website *without* providing credit card nr. Of course it has certain 
+limitations like space and CPU usage.
+
+You can also use it locally by running:
+`python flask_app.py` 
+
--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -8,39 +8,39 @@ from process_image import process_image
 app = Flask(__name__)
 
 # Get the absolute path to the 'static' folder
-static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-app.config['UPLOAD_FOLDER'] = os.path.join(static_folder, 'uploads')  # Set the upload folder for storing images
-
+static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+        'static')
+# Set the upload folder for storing images
+app.config['UPLOAD_FOLDER'] = os.path.join(static_folder, 'uploads')  
 
 @app.route('/')
-def index():
+def index() -> str:
+    '''Render the index.html template.'''
     return render_template('index.html')
 
 
 @app.route('/upload', methods=['POST'])
-def upload():
-
+def upload() -> str:
+    '''Handle the image upload and process the image.'''
     if 'image' in request.files:
+        # fetch relevant info from html 
         sigma = int(request.form['sigmaInput'])
         theta = int(request.form['thetaInput'])
         kernel = list(map(int, request.form['kernelSizeInput'].split('x')))
-        
         use_filter = request.form['yesNoOption'] == 'Yes'
         image = request.files['image']
-        image_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)  # Store the path of the uploaded image
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
 
-        os.makedirs(os.path.dirname(image_path), exist_ok=True)  # Create the directory if it doesn't exist
-
-        image.save(image_path)  # Save the uploaded image in the uploads folder
+        os.makedirs(os.path.dirname(image_path), exist_ok=True)  
+        image.save(image_path)  
 
         # Process the image and save the processed image
-        # Assuming you have a function called process_image() that takes the image path as input and returns the processed image path
         processed_image_path = process_image(image_path, theta,
                 use_filter, sigma, kernel)
 
         return render_template('index.html',
-                               image_path='uploads/' + basename(image_path),
-                               processed_image_path='uploads/' + basename(processed_image_path))
+                   image_path='uploads/' + basename(image_path),
+                   processed_image_path='uploads/' + basename(processed_image_path))
 
     return "No image uploaded"
 
